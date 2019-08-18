82e320c5d7c7f65fd0d18e100e17b172516d9e2b
AJ <ajlloyd98@gmail.com>
2019-08-18 19:08:00 +0100

    data preparation loop is functional (randomisation needed next)

--- xray_data_preparation.py
+++ xray_data_preparation.py
@@ -8,6 +8,7 @@ import pickle
 import numpy as np
 
 root = "./chest_xray"
+
 testing_data = []
 training_data = []
 validation_data = []
@@ -15,25 +16,20 @@ validation_data = []
 for ttv in os.listdir(root):                                                    # ttv = train, test, val folders
     data_set_dir = os.path.join(root, ttv)
     data_set_img_arrays = []                                                    # data set image array plus class
-
     for img_class in os.listdir(data_set_dir):
         img_path = os.path.join(data_set_dir,img_class)
         img_class = 0 if img_class == "NORMAL" else 1
-
         for img in os.listdir(img_path):
             img_array = cv2.imread(os.path.join(img_path,img), cv2.IMREAD_GRAYSCALE)
             new_array = cv2.resize(img_array,(100,100))
             data_set_img_arrays.append([new_array, img_class])
             print("image: {} complete".format(img))
-
     if ttv == "test":
         testing_data.append(data_set_img_arrays)
     elif ttv == "train":
         training_data.append(data_set_img_arrays)
     elif ttv == "val":
         validation_data.append(data_set_img_arrays)
-
-
 with open("./testing_data.pickle", 'wb') as file:
     pickle.dump(testing_data, file)
 with open("./training_data.pickle", 'wb') as file:
